250d3aea7b5e3fdb8a3190a637d671b8c941b3b9
parzival867 <nathyadiah@gmail.com>
2021-06-23 12:29:16 -0400

    Add code to display all posts on the homepage

--- app/blog.py
+++ app/blog.py
@@ -1,7 +1,7 @@
 import flask
 from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask import current_app as app
-from .models import Post
+from .models import Post, User
 from . import db
 from flask_login import login_required, current_user
 
@@ -10,4 +10,6 @@ bp = Blueprint('blog', __name__)
 @bp.route("/")
 @bp.route('/home')
 def index_page():
-	return render_template("blog/index.html")
+	# Show all the posts, most recent first
+	posts = db.session.query(Post.id, Post.title, Post.body, Post.created, Post.author_id, User.username).join(User).order_by(Post.created.desc()).all()
+	return render_template("blog/index.html", posts=posts)
